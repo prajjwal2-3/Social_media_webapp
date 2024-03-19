b75b4d72cca01c302d76e6270949dd1b909a8300
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-03-19 13:49:00 +0530

    signup end point connected from frontend

--- src/components/Signup.tsx
+++ src/components/Signup.tsx
@@ -1,7 +1,17 @@
 import React from 'react'
-
+import { useRef } from 'react'
+import axios from 'axios'
 const Signup = () => {
-  
+const name = useRef(null)
+
+const password = useRef(null)
+function signup(){
+       axios
+       .post("http://localhost:3001/user/signup",{username:"prajjwalsharma" ,password:"prajjwal@098",information:"react developer"})
+       .then((Response)=>{
+        console.log(Response)
+       })
+}
   return (
     <div className='flex h-screen '>
       <div className='w-6/12 p-20 bg-gray-200  '>
@@ -12,9 +22,9 @@ const Signup = () => {
         <div className='flex flex-col h-3/6 w-7/12 justify-between '>
         <div className='w-full text-center font-bold text-2xl'>LOGIN IN</div>
         
-       <input type="text" placeholder='Enter Name' className='rounded-lg p-4 bg-gray-200  '/>
-        <input type="text"   placeholder='Enter Password' className='rounded-lg p-4 bg-gray-200 '/>
-        <button className='bg-blue-500 p-2 rounded-lg '>Sign Up</button>
+       <input ref={name} type="text" placeholder='Enter Name' className='rounded-lg p-4 bg-gray-200  '/>
+        <input ref={password} type="text"   placeholder='Enter Password' className='rounded-lg p-4 bg-gray-200 '/>
+        <button className='bg-blue-500 p-2 rounded-lg  ' onClick={signup}>Sign Up</button>
         </div>
       </div>
       
