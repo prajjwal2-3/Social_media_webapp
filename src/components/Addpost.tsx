84b0a4285e3eb3ab1ac621da5be34341a2047c99
prajjwal2-3 <121849327+prajjwal2-3@users.noreply.github.com>
2024-03-26 15:55:18 +0530

    build optimised

--- src/components/Addpost.tsx
+++ src/components/Addpost.tsx
@@ -30,15 +30,18 @@ interface user {
   function addpost(){
    if(post.current){
     const data = post.current.value
-    axios.post('https://social-backend-navy.vercel.app/user/post',{description:data},{headers:headers}).then(()=>{alert('post updated successfully')})
+    axios.post('https://social-backend-navy.vercel.app/user/post',{description:data},{headers:headers}).then(()=>{
+      alert('post updated successfully');
+     
+  })
    }
   }
   
-  
+ 
   return (
     <div className='bg-gray-200 h-20 rounded-xl flex items-center justify-evenly p-4'>
       <div className=''>
-        <input type="text" ref={post} placeholder='Share something with the world' className='h-12 sm:w-96  rounded-lg p-2' />
+        <input type="text" ref={post} id='myi' placeholder='Share something with the world' className='h-12 sm:w-96  rounded-lg p-2' />
       </div>
       <div className="" >
         <button onClick={addpost} className='bg-blue-500 p-2 m-1 rounded-md'>Post</button>
